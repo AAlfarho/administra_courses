1a34d2af5b704ad116ab7c2749b1f2606ff34ac5
Alfaro <st.alfaro@gmail.com>
2017-07-18 17:13:35 +0000

    composable update of authors

---
+++ src/components/authors/authorList.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import React from 'react';
+
+export default class AuthorList extends React.Component {
+    
+    createAuthorRow(author) {
+        return (
+            <tr key={author.id}>
+                <td><a href={'/#authors/' + author.id}>{author.id}</a></td>
+                <td>{author.firstName} {author.lastName}</td>
+            </tr>
+            );
+    }
+    render() {
+        return (
+            <div>
+                <table className='table'>
+                    <thead>
+                        <th>ID</th>
+                        <th>Name</th>
+                    </thead>
+                    <tbody>
+                        {this.props.authors.map(this.createAuthorRow, this)}
+                    </tbody>
+                </table>
+            </div>
+            );
+    }
+}
--- src/components/authors/authors.js
+++ src/components/authors/authors.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import AuthorAPI from '../../api/authorAPI';
+import AuthorList from './authorList';
 
 export default class Authors extends React.Component {
     constructor(props){
@@ -9,33 +10,17 @@ export default class Authors extends React.Component {
         this.state = {authors: []};
     }
 
-    componentWillMount() {
+    componentDidMount() {
         this.setState({
             authors: AuthorAPI.getAllAuthors()
-        });
+        });  
     }
     
-    createAuthorRow(author) {
-        return (
-            <tr key={author.id}>
-                <td><a href={'/#authors/' + author.id}>{author.id}</a></td>
-                <td>{author.firstName} {author.lastName}</td>
-            </tr>
-            );
-    }
     render() {
         return (
             <div>
                 <h1>Authors</h1>
-                <table className='table'>
-                    <thead>
-                        <th>ID</th>
-                        <th>Name</th>
-                    </thead>
-                    <tbody>
-                        {this.state.authors.map(this.createAuthorRow, this)}
-                    </tbody>
-                </table>
+                <AuthorList authors={this.state.authors} />
             </div>
             );
     }
