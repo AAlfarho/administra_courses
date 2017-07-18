46bb27c67d5607636e9da76ed35858cebf30e775
Alfaro <st.alfaro@gmail.com>
2017-07-18 23:30:47 +0000

    react router implemented for nav bar

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var React = require('react');
-
+//var React = require('react');
+import React from 'react';
+import {Link} from 'react-router';
 
 export default class Home extends React.Component {
      render() {
@@ -9,6 +10,8 @@ export default class Home extends React.Component {
            <div className='jumbotron'>
                 <div className='container'>
                     <h1>Pluralsight</h1>
+                    <p>Dummy website for course and authors mgmt</p>
+                    <Link to='about' className='btn btn-primary btn-lg'>Learn more</Link>
                 </div>
             </div>
              );
