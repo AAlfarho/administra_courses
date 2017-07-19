f7006137a16818f167e8da0c0aaad7de7192d477
Alfaro <st.alfaro@gmail.com>
2017-07-19 00:34:51 +0000

    transition to n from example

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -3,6 +3,21 @@
 import React from 'react';
 
 export default class About extends React.Component {
+    
+    //This is no longer valid on react-router 1.x, instead we use Route on enter.
+    // static willTransitionTo(transition, params, query, callback) {
+    //     if(!confirm('Read about?')){
+    //         transition.abort();
+    //     } else {
+    //         callback();
+    //     }
+    // }
+    
+    //     static willTransitionFrom(transition, component) {
+    //     if(!confirm('Dont leave!?')){
+    //         transition.abort();
+    //     }
+    // }
     render() {
         return (
             <div>
